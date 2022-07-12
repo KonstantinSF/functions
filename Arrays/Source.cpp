a3440918d8c57346c8c357b0c1db84667ec487de
KonstantinSF <kosmos008@gmail.com>
2022-07-12 21:30:25 +0300

    add  fillrand, print, sort function

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -1,8 +1,42 @@
+//Arrays
 #include<iostream>
 using namespace std;
+#define tab "\t"
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
 
 void main()
 {
 	setlocale(LC_ALL, " ");
 
+}
+
+void FillRand(int arr[], const int n)//код заполнения массива случ числами
+{
+	for (int i = 0; i < n; i++) arr[i] = rand() % 100;
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
 }
